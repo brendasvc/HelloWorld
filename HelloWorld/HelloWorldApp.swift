cda05706690eb8b5ddcca5a5b71ff98e469d68e6
brenda <bvega96@outlook.com>
2024-04-02 18:28:20 -0700

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Arturo De la Pena on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
